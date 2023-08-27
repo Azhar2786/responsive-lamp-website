33adbe51353770130ffe6488afee4696459eb37a
Azhar <darkcoders0101@gmail.com>
2023-08-27 23:19:46 +0530

    updating fiiles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,63 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/* ============== Menu Show ================*/
+/* Validate if constent exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* ============== Menu Hidden ================*/
+/* Validate if constent exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on each nav__link, we remove the show-mwnu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    //when the scroll is greater than 50 viewport height, add the bg-header class to
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
+
+const popularSwiper = new Swiper('.popular__content', {
+    // Optional parameters
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        768: {
+            centeredSlides: false,
+        }
+    }
+  });
 
 
 /*=============== CHOOSE FAQ ===============*/
